5cafff5eb82644288d34c3782e0222634511f7e5
Bradly Chang <bradly@cypone.dev>
2026-07-12 01:00:35 +0800

    chore: configure vue-i18n-extract.config.js and simplify package.json script

---
+++ vue-i18n-extract.config.js
@@ -0,0 +1,21 @@
+export const vueFiles = "./app/**/*.?(vue|ts)";
+export const languageFiles = "./i18n/locales/*.json";
+export const exclude = [
+  "seo",
+  "home",
+  "menu",
+  "apply",
+  "tba",
+  "ticket",
+  "newsPage",
+  "news",
+  "common",
+  "schedule",
+  "venue",
+];
+export const output = false;
+export const add = false;
+export const remove = false;
+export const ci = true;
+export const separator = ".";
+export const noEmptyTranslation = "";
